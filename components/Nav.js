98186a25b318200dca9f5a578f5bf1d9835a92fa
dkotsev <dobromir.kotsev@gmail.com>
2022-06-01 11:21:48 +0300

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,124 @@
+function Nav() {
+    return ( 
+<nav className="flex items-center justify-around w-full bg-zinc-700">
+  <a
+    className="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg"
+    href="/"
+  >
+    
+  </a>
+
+  <ul className="flex items-center space-x-2 text-sm font-medium text-white">
+    <li className="hidden lg:block text-white ">
+
+    
+      <a className="px-2 py-2 rounded-lg inline-flex items-center" href="/"> 
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+         <g id="Group_746" data-name="Group 746" transform="translate(-1290 -10)">
+         <g id="Group_617" data-name="Group 617" transform="translate(883.094 -974.102)">
+          <ellipse id="Ellipse_80" data-name="Ellipse 80" cx="12" cy="11.991" rx="12" ry="11.991" transform="translate(406.906 984.102)" fill="#00aeef"/>
+         <path id="Path_128" data-name="Path 128" d="M417.73,1004.293a11.653,11.653,0,0,1-7.995-3.455c.011-.045,3.359-2.449,4.88-2.251,0,0,.944,1.044,3.547,1.044s3.546-1.045,3.546-1.045c1.528-.2,3.267.919,4.88,2.24a11.842,11.842,0,0,1-8.857,3.466" transform="translate(0.744 3.792)" fill="#f8f8f8"/>
+        <g id="Group_616" data-name="Group 616" transform="translate(414.129 989.302)">
+        <path id="Path_129" data-name="Path 129" d="M421.952,991.666a4.771,4.771,0,0,0-4.535-3.443h-.057a4.732,4.732,0,0,0-4.684,4.2,4.9,4.9,0,0,0-.053.712c0,2.636,2.139,6.175,4.777,6.175s4.775-3.539,4.775-6.175a4.756,4.756,0,0,0-.239-1.5Z" transform="translate(-412.624 -988.222)" fill="#f8f8f8"/>
+         </g>
+         </g>
+        </g>
+    </svg>
+      
+     <p className="pl-1">   БЮЛЕТИНИ </p>
+      
+      </a>
+    </li>
+
+    <li className="hidden lg:block" > 
+        <a className="px-2 py-2 rounded-lg text-white inline-flex items-center" href="" > 
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+  <circle id="Ellipse_406" data-name="Ellipse 406" cx="11" cy="11" r="11" fill="#fff"/>
+</svg>
+
+    
+    
+    
+<p className="pl-1"> МОИТЕ СТАТИИ </p>
+</a></li>
+
+<li className="hidden lg:block" > 
+      <a
+        className="inline-flex items-center px-2 py-2 rounded-lg text-white"
+        href=""
+        target="_blank"
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <g id="Group_743" data-name="Group 743" transform="translate(-150 -8)">
+    <circle id="Ellipse_88" data-name="Ellipse 88" cx="11" cy="11" r="11" transform="translate(151 9)" fill="#f8f8f8"/>
+    <g id="Group_742" data-name="Group 742" transform="translate(-2 -2)">
+      <g id="Group_738" data-name="Group 738" transform="translate(152 10)">
+        <circle id="Ellipse_95" data-name="Ellipse 95" cx="3.224" cy="3.224" r="3.224" transform="translate(6.748 6.722) rotate(-0.734)" fill="#00aeef"/>
+        <path id="Path_143" data-name="Path 143" d="M604,304.03a12,12,0,1,0,12,12A12,12,0,0,0,604,304.03Zm5.445,19.544L604.6,318.73a5.48,5.48,0,1,1,2.044-1.936l4.79,4.789Z" transform="translate(-592 -304.03)" fill="#00aeef"/>
+      </g>
+    </g>
+  </g>
+</svg>
+
+
+
+
+        <p className="pl-1">ТЪРСЕНЕ</p>
+       
+      </a>
+    </li>
+
+    <li>
+      <a
+        className="inline-flex items-center px-2 py-2 rounded-lg text-white"
+        href=""
+        target="_blank">
+          <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+         <g id="Group_746" data-name="Group 746" transform="translate(-1290 -10)">
+         <g id="Group_617" data-name="Group 617" transform="translate(883.094 -974.102)">
+          <ellipse id="Ellipse_80" data-name="Ellipse 80" cx="12" cy="11.991" rx="12" ry="11.991" transform="translate(406.906 984.102)" fill="#00aeef"/>
+         <path id="Path_128" data-name="Path 128" d="M417.73,1004.293a11.653,11.653,0,0,1-7.995-3.455c.011-.045,3.359-2.449,4.88-2.251,0,0,.944,1.044,3.547,1.044s3.546-1.045,3.546-1.045c1.528-.2,3.267.919,4.88,2.24a11.842,11.842,0,0,1-8.857,3.466" transform="translate(0.744 3.792)" fill="#f8f8f8"/>
+        <g id="Group_616" data-name="Group 616" transform="translate(414.129 989.302)">
+        <path id="Path_129" data-name="Path 129" d="M421.952,991.666a4.771,4.771,0,0,0-4.535-3.443h-.057a4.732,4.732,0,0,0-4.684,4.2,4.9,4.9,0,0,0-.053.712c0,2.636,2.139,6.175,4.777,6.175s4.775-3.539,4.775-6.175a4.756,4.756,0,0,0-.239-1.5Z" transform="translate(-412.624 -988.222)" fill="#f8f8f8"/>
+         </g>
+         </g>
+        </g>
+</svg>
+<span className="flex h-3 w-3 relative p-0">
+<span className=" absolute -top-[6px] -left-[10px] animate-ping absolute inline-flex h-2 w-2 rounded-full bg-red-100 opacity-75"></span>
+<span className="  absolute -top-[6px] -left-[10px] inline-flex rounded-full h-2 w-2 bg-rose-600"></span>  
+</span>
+<p className="pl-0 relative hover:text-sky-600">ВЛЕЗ В ПРОФИЛА СИ</p>
+
+       
+
+      </a>
+    </li>
+
+    <li className="hidden lg:block">
+      <a
+        className="inline-flex items-center px-3 py-2 rounded-lg text-white"
+        href=""
+        target="_blank"
+      >
+       
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <g id="Group_745" data-name="Group 745" transform="translate(-152 -10)">
+            <circle id="Ellipse_88" data-name="Ellipse 88" cx="9.6" cy="9.6" r="9.6" transform="translate(154.4 12.4)" fill="#f8f8f8"/>
+            <path id="Path_130" data-name="Path 130" d="M12,0A12,12,0,1,0,24,12,12,12,0,0,0,12,0Zm4.836,14.425L12.391,18.87,9.966,16.445l2.73-2.73H4.2V10.285h8.5l-2.73-2.73L12.391,5.13l4.445,4.445L19.261,12Z" transform="translate(152 34) rotate(-90)" fill="#00aeef"/>
+             </g>
+        </svg>
+      
+        <p className="pl-1">
+        АБОНАМЕНТ
+       </p>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+     );
+}
+
+export default Nav;
