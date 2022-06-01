75ca3009442a19a769accbaa6d4608a328c6e106
dkotsev <dobromir.kotsev@gmail.com>
2022-06-01 11:32:24 +0300

    compinetns fixk

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Nav from '../components/nav'
+import Nav from '../components/Nav'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
