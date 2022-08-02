711c97246c0b148179aa06f158301e3f83fa1a0b
dkotsev <dobromir.kotsev@gmail.com>
2022-08-02 14:18:05 +0300

    airtable form

---
+++ components/Form.js
@@ -0,0 +1,16 @@
+function Form() {
+    return ( 
+      <div className="flex justify-center">
+            <iframe class="airtable-embed" src="https://airtable.com/embed/shranNZ8naMCySj06?backgroundColor=blue" frameborder="0" onmousewheel="" width="50%" height="533">
+                
+            </iframe>
+        
+
+    </div>
+
+
+
+     );
+    }
+    
+    export default Form;
--- pages/article.js
+++ pages/article.js
@@ -2,13 +2,14 @@ import Nav from '../components/Nav'
 import BottomBar1 from '../components/BottomBar1';
 import BottomBar2 from '../components/BottomBar2';
 import BottomBar3 from '../components/BottomBar3';
+import Form from '../components/Form';
 
 export default function article() {
 return (
     <div>
      
       <Nav/>
-      
+      <Form/>
       <BottomBar2/>
       
      
