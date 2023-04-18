860a258701f128b272946b69a5ab85200b9479d6
dkotsev <dobromir.kotsev@gmail.com>
2023-04-18 17:58:26 +0300

    fixxing article 2.js

--- pages/article.js
+++ pages/article.js
@@ -2,7 +2,6 @@ import Nav from '../components/Nav'
 import BottomBar1 from '../components/BottomBar1';
 import BottomBar2 from '../components/BottomBar2';
 import BottomBar3 from '../components/BottomBar3';
-import Form from '../components/Form';
 
 export default function article() {
 return (
