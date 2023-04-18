f6b4db01b7f8b36925a21ff84277e8f0910d2993
dkotsev <dobromir.kotsev@gmail.com>
2023-04-18 16:55:59 +0300

    fix error 2

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       </div>
 
       <div >
-      <iframe className="airtable-embed" src="https://airtable.com/embed/shrcWM2emcSn9OoBK?backgroundColor=yellow&viewControls=on" frameBorder="0" onmousewheel="" width="100%" height="533"></iframe>
+      <iframe className="airtable-embed" src="https://airtable.com/embed/shrcWM2emcSn9OoBK?backgroundColor=yellow&viewControls=on" frameBorder="1" onmouseWheel="" width="80%" height="533"></iframe>
 
       </div>
       
